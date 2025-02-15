9e93b7d569d4ebb1360f437d07c6ddfbc19e5494
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2025-02-16 00:08:16 +1300

    Javadoc Main.java

--- src/main/java/chiefarug/projects/mixinattempt/Main.java
+++ src/main/java/chiefarug/projects/mixinattempt/Main.java
@@ -1,5 +1,8 @@
 package chiefarug.projects.mixinattempt;
 
+/**
+* The main class, this is just a target for mixining into, nothing special.
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
